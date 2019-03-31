dd9f2e97ba849ae69fb4bd66de1ba34b4ad691e6
rocky <rocky.harden@qq.com>
2019-03-31 16:46:16 +0800

    modify basic

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import AppState from './AppState';
-import App from './App';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-const appState = new AppState();
-
-render(
-  <AppContainer>
-    <App appState={appState} />
-  </AppContainer>,
-  document.getElementById('root')
-);
-
-if (module.hot) {
-  module.hot.accept('./App', () => { render(App) })
-}
+ReactDom.render(<Router />,document.getElementById('root'))
